b8e9b93d42a186084389fcf105492b6642bb9421
Moritz Haseloff <haseloff@acentiss.de>
2020-04-23 23:58:14 +0200

    update plant class with watering calculation

--- plant.cpp
+++ plant.cpp
@@ -1,7 +1,7 @@
 #include "plant.h"
 
 
-Plant::Plant(const char* plantname, const unsigned long mlmin, const unsigned long mlmax) : _name(plantname), _daily_amount_min_ml(mlmin), _daily_amount_max_ml(mlmax)
+Plant::Plant(const char* plantname, const unsigned long mlmin, const unsigned long mlmax, const unsigned long mlmaxpot) : _name(plantname), _daily_amount_min_ml(mlmin), _daily_amount_max_ml(mlmax), _max_per_watering(mlmaxpot)
 {
     
 }
@@ -20,6 +20,19 @@ unsigned long Plant::getDailyMin() const
 unsigned long Plant::getDailyMax() const
 {
   return _daily_amount_max_ml;
+}
+
+unsigned long Plant::calcWaterAmout(double temp, double humidity, int clouds, unsigned long secs_p_day) const
+{
+  //can be called several times a day, calc watering amout today, minus water given today limited by pot size
+  //main loop needs to call all plants in the evening if they had enough for today and water again if not TODO
+  //main loop keeps track of timing since only one plant is watered at a time. ~ millis() - currentplant_started_millis * 1000 * mlps < calcWateramount
+  double ret = (double)_daily_amount_min_ml + (double)(_daily_amount_max_ml-_daily_amount_min_ml)*(0.5*(1-((double)clouds)/100.0) + 0.5*(1-humidity/100.0))*(temp/15.0)*((double)secs_p_day /3600.0 /12.0) - (double) _daily_amount_ml;
+  if(ret > _max_per_watering)
+  {
+    ret = _max_per_watering;
+  }
+  return (unsigned long) ret;
 }
 
 
--- plant.h
+++ plant.h
@@ -9,14 +9,16 @@ class Plant
     String _name;
     unsigned long _daily_amount_max_ml;
     unsigned long _daily_amount_min_ml;
+    unsigned long _max_per_watering;
     unsigned long _daily_amount_ml = 0;
     unsigned long _total_amount_ml = 0;
     
     public:
-    Plant(const char* plantname, const unsigned long mlmin, const unsigned long mlmax);
+    Plant(const char* plantname, const unsigned long mlmin, const unsigned long mlmax, const unsigned long mlmaxpot);
     const char* getName() const;
     unsigned long getDailyMin() const;
     unsigned long getDailyMax() const;
+    unsigned long calcWaterAmout(double temp, double humidity, int clouds, unsigned long secs_p_day) const;
     unsigned long getDailyWater() const;
     unsigned long getTotalWater() const;
     void resetDailyWater();
